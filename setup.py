f446a191ec0b3bb3bfeb049bbed503614f2596d9
Your Name <you@example.com>
2024-04-30 17:29:29 -0700

    more nix stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="cs158b-project",
+    version="1.0",
+    packages=find_packages(),
+    scripts=["app.py"],
+)
